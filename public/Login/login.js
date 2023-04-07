393b578a2605a1aa7acfe847d38d26b3b4373e91
Bichchuran10 <bichchuran10@gmail.com>
2023-04-07 15:36:03 +0530

    login frontend

---
+++ public/Login/login.js
@@ -0,0 +1,24 @@
+const login=async(event)=>
+{
+    try{
+    event.preventDefault()
+    let email=event.target.email.value
+    let password=event.target.password.value
+
+    const loginDetails={
+        email,
+        password
+    }
+
+    let response=await axios.post('http://localhost:3000/user/login',loginDetails)
+    if(response.status==200){   
+        alert('User login successfull') 
+    }
+}
+    catch(err)
+    {
+            console.log(err);
+            document.body.innerHTML=`<div style="color:red;">${err.message} <div>`
+
+    }
+}
