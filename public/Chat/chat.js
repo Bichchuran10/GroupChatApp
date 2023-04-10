e0878d319de455f8d050a353f14f6271bcbdc407
Bichchuran10 <bichchuran10@gmail.com>
2023-04-10 14:39:56 +0530

    chatApp real time

--- public/Chat/chat.js
+++ public/Chat/chat.js
@@ -21,9 +21,36 @@ document.getElementById('chat-form').onsubmit = async (event) => {
 
 window.addEventListener('DOMContentLoaded', async () => {
     try {
-        const res = await axios.get('http://localhost:3000/message/fetch');
+        setInterval(() => {
+            fetchMessages();
+        }, 1000);  
     } 
     catch (error) {
         console.log(error);
     }
 });
+
+
+const fetchMessages=async()=>{
+
+    const res = await axios.get('http://localhost:3000/message/fetch');
+    console.log("dataaaaaa",res.data)
+    const messages = res.data.message;
+    showChat(messages);
+
+}
+
+function showChat(messages)
+{
+    try{
+        const chatBody = document.getElementById('chat-body');
+        chatBody.innerHTML = '';
+        messages.forEach((message) => {
+            chatBody.innerHTML += message.message + `<br>`;
+        });
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
